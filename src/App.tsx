eec0b74953f0c8923b243b74dc8d7a3ebc4f7661
gosho-kazuya <ketsume0211@gmail.com>
2018-08-17 10:50:40 +0900

    add tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
 
+import * as SendBird from 'sendbird'
+
+const sb = new SendBird({
+  appId: '9F0BAC98-572F-457B-B0CE-55F334A17550',
+})
+
 interface State {}
 
 export default class App extends React.Component<{}, State> {
